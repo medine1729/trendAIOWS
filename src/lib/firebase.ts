4d154a0ddf5091f304a916897dec4efbcf59ddf7
Medine Kaynak <medinekaynak2906@gmail.com>
2026-04-04 00:58:56 +0300

    fix(firebase): handle missing undefined keys to prevent prerendering crash

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "dummy-key-to-prevent-build-crash",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
